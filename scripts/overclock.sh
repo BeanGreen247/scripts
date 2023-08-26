14fdfaa93c7b468aa3d168d9b235683bf46c2ea7
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2023-08-26 08:22:31 +0200

    Update overclock.sh

--- scripts/overclock.sh
+++ scripts/overclock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nvidia-settings -a '[gpu:0]/GPUGraphicsClockOffset[1]=100'
-nvidia-settings -a '[gpu:0]/GPUGraphicsMemoryOffset[1]=1000'
-nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[1]=1000'
-nvidia-settings -a '[gpu:0]/GPUFanControlState=1'
-nvidia-settings -a '[fan:0]/GPUTargetFanSpeed=70'
+sudo nvidia-settings -a '[gpu:0]/GPUGraphicsClockOffset[1]=100'
+sudo nvidia-settings -a '[gpu:0]/GPUGraphicsMemoryOffset[1]=1000'
+sudo nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[1]=1000'
+sudo nvidia-settings -a '[gpu:0]/GPUFanControlState=1'
+sudo nvidia-settings -a '[fan:0]/GPUTargetFanSpeed=70'
