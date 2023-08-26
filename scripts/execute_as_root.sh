648868bbb23364527733068b13619e17c27838d6
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2023-08-26 06:52:11 +0200

    Add files via upload

--- scripts/execute_as_root.sh
+++ scripts/execute_as_root.sh
@@ -2,3 +2,4 @@
 pass=$(zenity --password --title="Enter root Password")
 echo $pass | sudo -S bash overclock.sh
 echo $pass | sudo -S bash standby_hdd.sh
+echo $pass | sudo -S bash performance-mode.sh
