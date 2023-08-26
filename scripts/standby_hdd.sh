024a246e239847f79713188c7f116b5e393c5844
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2023-08-26 08:18:32 +0200

    Update standby_hdd.sh

--- scripts/standby_hdd.sh
+++ scripts/standby_hdd.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-disk_models=$(grep . /sys/class/block/sd*/device/model)
-while read -r line; do
-  disk_name=$(echo "$line" | cut -d "/" -f 5)
-  model_name=$(echo "$line" | cut -d ":" -f 2)
-  if [[ "$model_name" == *"SSD"* ]]; then
-    continue
-  else
-    if dpkg -s hdparm >/dev/null 2>&1; then
-   	sudo hdparm -Y /dev/$(echo "${disk_name##*/}")
-    else
-   	sudo apt-get update
-   	sudo apt-get install -y hdparm
-   	sudo hdparm -Y /dev/$(echo "${disk_name##*/}")
-   fi
-  break
-  fi
-done <<< "$disk_models"
+disk_name=$(lsblk -d -o name,rota | awk '$2 == "1" && $1 !~ /^sr/ {print $1; exit}')
+if dpkg -s hdparm >/dev/null 2>&1; then
+   sudo hdparm -Y /dev/$(echo "${disk_name##*/}")
+else
+   sudo apt-get update
+   sudo apt-get install -y hdparm
+   sudo hdparm -Y /dev/$(echo "${disk_name##*/}")
+fi
